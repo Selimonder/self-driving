1323841141ba125189794d633888b693b0c9b578
root <root@DESKTOP-JM5I85D.localdomain>
2020-09-30 16:43:57 +0200

    3d

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os, sys, time
-os.environ['KMP_DUPLICATE_LIB_OK']='False'
+# os.environ['KMP_DUPLICATE_LIB_OK']='False'
 
 import torch
 from torch import nn, optim
@@ -29,6 +29,9 @@ DIR_INPUT = "../../input/lyft-motion-prediction-autonomous-vehicles"
 SINGLE_MODE_SUBMISSION = f"{DIR_INPUT}/single_mode_sample_submission.csv"
 MULTI_MODE_SUBMISSION = f"{DIR_INPUT}/multi_mode_sample_submission.csv"
 
+# set env variable for data
+os.environ["L5KIT_DATA_FOLDER"] = DIR_INPUT
+
 DEBUG = False
 
 cfg = {
@@ -44,8 +47,8 @@ cfg = {
     },
     
     'raster_params': {
-        'raster_size': [128, 128],  ## 300 300
-        'pixel_size': [0.33, 0.33], ## 0.5 0.5
+        'raster_size': [160, 160],  ## 300 300
+        'pixel_size': [0.32, 0.32], ## 0.5 0.5
         'ego_center': [0.25, 0.5],
         'map_type': 'py_semantic',
         'satellite_map_key': 'aerial_map/aerial_map.png',
@@ -63,9 +66,9 @@ cfg = {
 
     'val_data_loader': {
         'key': 'scenes/validate.zarr',
-        'batch_size': 8,
+        'batch_size': 128,
         'shuffle': False,
-        'num_workers': 0,
+        'num_workers': 2,
     },
     
     'train_params': {
@@ -75,9 +78,6 @@ cfg = {
         # 'eval_every_n_steps': -1
     }
 }
-
-# set env variable for data
-os.environ["L5KIT_DATA_FOLDER"] = DIR_INPUT
 
 class CheckpointEveryNSteps(pl.Callback):
     """
@@ -119,28 +119,34 @@ class CheckpointEveryNSteps(pl.Callback):
 if __name__ == '__main__':
     print("starting")
     callbacks = [
-        pl.callbacks.LearningRateLogger('step'),
         CheckpointEveryNSteps(save_step_frequency=2500)
     ]
 
     train_dataset = MotionPredictDataset(cfg)
+    train_cfg = cfg['train_data_loader']
     print("train_dataset ok")
-    train_loader = DataLoader(train_dataset, batch_size=cfg['train_data_loader']['batch_size'], 
-                             num_workers=cfg['train_data_loader']['num_workers'], shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=train_cfg['batch_size'], 
+                             num_workers=train_cfg['num_workers'], shuffle=train_cfg['shuffle'], pin_memory=False)
+
+
+    # val_dataset = MotionPredictDataset(cfg, str_loader="val_data_loader")
+    # print("val_data_ok ok")
+    # val_loader = DataLoader(val_dataset, batch_size=cfg['val_data_loader']['batch_size'], 
+    #                          num_workers=cfg['val_data_loader']['num_workers'], shuffle=False)
 
     
-    ## test_mask_path = os.path.join(DIR_INPUT, 'scenes/mask.npz') for test
+    # ## test_mask_path = os.path.join(DIR_INPUT, 'scenes/mask.npz') for test
     
 
-    t_start = time.time()
-    for i, batch in enumerate(train_loader):
-        if i == 16:
-            break
-        print(i, batch['image'].shape)
+    # t_start = time.time()
+    # for i, batch in enumerate(train_loader):
+    #     if i == 16:
+    #         break
+    #     print(i, batch['image'].shape)
 
-    t_end = time.time()
-    t_total = t_end - t_start
-    print(t_total)
+    # t_end = time.time()
+    # t_total = t_end - t_start
+    # print(t_total)
 
     # ==== INIT MODEL
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -154,10 +160,20 @@ if __name__ == '__main__':
                 layer.bias.data.fill_(0.)
 
 
+    from src.loss import *
+    from linformer_pytorch import Linformer
+    from effnet import *
+
+    import torchvision
+    #import torch_optimizer as optim
+    from adamp import AdamP
+    
+
     class LyftModel(pl.LightningModule):
-        def __init__(self, cfg: Dict, num_modes=3):
+        def __init__(self, cfg: Dict, learning_rate = 5e-5, num_modes=3):
             super().__init__()
 
+            #self.learning_rate = learning_rate
             ## c
             num_history_channels = (cfg["model_params"]["history_num_frames"] + 1) * 2
             num_in_channels = 3 + num_history_channels
@@ -166,103 +182,100 @@ if __name__ == '__main__':
             self.future_len = cfg["model_params"]["future_num_frames"]
             num_targets = 2 * self.future_len
 
-            # TODO: support other than resnet18?
-            self.backbone = EfficientNet.from_pretrained('efficientnet-b1', in_channels=num_in_channels)
-            backbone_out_features = self.backbone._fc.weight.shape[1]
 
-            self.linformer = Linformer(
-            input_size=2, # Dimension 1 of the input
-            channels=1280, # Dimension 2 of the input
-            dim_d=64, # Overwrites the inner dim of the attention heads. If None, sticks with the recommended channels // nhead, as in the "Attention is all you need" paper
-            dim_k=128, # The second dimension of the P_bar matrix from the paper
-            dim_ff=128, # Dimension in the feed forward network
-            dropout_ff=0.15, # Dropout for feed forward network
-            nhead=4, # Number of attention heads
-            depth=2, # How many times to run the model
-            dropout=0.1, # How much dropout to apply to P_bar after softmax
-            activation="gelu", # What activation to use. Currently, only gelu and relu supported, and only on ff network.
-            checkpoint_level="C0", # What checkpoint level to use. For more information, see below.
-            parameter_sharing="layerwise", # What level of parameter sharing to use. For more information, see below.
-            k_reduce_by_layer=0, # Going down `depth`, how much to reduce `dim_k` by, for the `E` and `F` matrices. Will have a minimum value of 1.
-            full_attention=False, # Use full attention instead, for O(n^2) time and space complexity. Included here just for comparison
-            include_ff=True, # Whether or not to include the Feed Forward layer
-            w_o_intermediate_dim=None, # If not None, have 2 w_o matrices, such that instead of `dim*nead,channels`, you have `dim*nhead,w_o_int`, and `w_o_int,channels`
-            )
+            # avg pool
+            self.backbone = torchvision.models.video.r3d_18(pretrained=True, progress=True)
+            self.backbone.stem[0] = nn.Conv3d(
+                25,
+                64,
+                kernel_size=(3, 7, 7),
+                stride=(1, 2, 2),
+                padding=(1, 3, 3),
+                bias=False)
 
-            # You can add more layers here.
-            self.head = nn.Sequential(
-                # nn.Dropout(0.2),
-                nn.Linear(in_features=2560, out_features=2560),
-            )
 
             self.num_preds = num_targets * num_modes
             self.num_modes = num_modes
 
-            self.logit = nn.Linear(2560, out_features=self.num_preds + num_modes)
+            self.logit = nn.Linear(512, out_features=self.num_preds + num_modes)
 
         def init_weight():
             init_layer(self.logit)
 
-        def forward(self, x):
-            x = self.backbone(x)
-            x = F.avg_pool2d(x, kernel_size=(2, 2))
-            x = F.dropout(x, p=0.2, training=self.training)        
-            x = torch.mean(x, dim=3)
-            x1 = F.max_pool1d(x, kernel_size=3, stride=1, padding=1)
-            x2 = F.avg_pool1d(x, kernel_size=3, stride=1, padding=1)
-            x = x1 + x2
-
-            #x = F.dropout(x, p=0.3, training=self.training)
-            x = x.transpose(1, 2)
-            x = self.linformer(x)
-            x = x.transpose(1, 2)
-
+        def forward(self, images, matrix, centroid, infer=False):
+                    
+            x = self.backbone.stem(images)
+            x = self.backbone.layer1(x)
+            x = self.backbone.layer2(x)
+            x = self.backbone.layer3(x)
+            x = self.backbone.layer4(x)
+            x = self.backbone.avgpool(x)
             x = torch.flatten(x, 1)
             x = F.dropout(x, p=0.3, training=self.training)
             x = self.logit(x)
-            
+
             # pred (bs)x(modes)x(time)x(2D coords)
             # confidences (bs)x(modes)
             bs, _ = x.shape
+            tl = self.future_len
+            
             pred, confidences = torch.split(x, self.num_preds, dim=1)
             pred = pred.view(bs, self.num_modes, self.future_len, 2)
             assert confidences.shape == (bs, self.num_modes)
             confidences = torch.softmax(confidences, dim=1)
+            
+            if infer:
+                matrix_inv = torch.inverse(matrix)
+                pred = pred + bias[:,None,:,:]
+                pred = torch.cat([pred,torch.ones((bs,3,tl,1)).to(device)], dim=3)
+                pred = torch.stack([torch.matmul(matrix_inv.to(torch.float), pred[:,i].transpose(1,2)) 
+                                    for i in range(3)], dim=1)
+                pred = pred.transpose(2,3)[:,:,:,:2]
+                pred = pred - centroid[:,None,:,:]
+                
             return pred, confidences
 
         def training_step(self, batch, batch_idx):
             data = batch
-            inputs = data["image"].to(device)
+            
+            ## batch
+            inputs = data["image"].unsqueeze(2).to(device)
             target_availabilities = data["target_availabilities"].to(device)
             targets = data["target_positions"].to(device)
-
-            pred, confidences = self(inputs)
+            matrix = data["world_to_image"].to(device)
+            centroid = data["centroid"].to(device)[:,None,:].to(torch.float)
+            
+            ## forward
+            pred, confidences = self(inputs, matrix, centroid)
+            
+            
+            ## fix
+            bs,tl,_ = targets.shape
+            assert tl == cfg["model_params"]["future_num_frames"]
+            targets = targets + centroid
+            targets = torch.cat([targets,torch.ones((bs,tl,1)).to(device)], dim=2)
+            targets = torch.matmul(matrix.to(torch.float), targets.transpose(1,2))
+            targets = targets.transpose(1,2)[:,:,:2]
+            bias = torch.tensor([40.0, 80.0])[None,None,:].to(device)
+            targets = targets - bias
+            
+            ## back
             loss = pytorch_neg_multi_log_likelihood_batch(targets, pred, confidences, target_availabilities)
 
             result = pl.TrainResult(loss)
             result.log('train_loss', loss, on_epoch=True)
             return result
 
-        # def validation_step(self, batch, batch_idx):
-        #     data = batch
-        #     inputs = data["image"].to(device)
-        #     target_availabilities = data["target_availabilities"].to(device)
-        #     targets = data["target_positions"].to(device)
-
-        #     pred, confidences = self(inputs)
-        #     loss = pytorch_neg_multi_log_likelihood_batch(targets, pred, confidences, target_availabilities)
-
-        #     result = pl.EvalResult(checkpoint_on=loss)
-        #     result.log('val_loss', loss)
-        #     return result
 
         def configure_optimizers(self):
-            optimizer = torch.optim.AdamW(self.parameters(), lr=1e-5)
-            #self.scheduler = torch.optim.lr_scheduler.OneCycleLR(
-            #                            self.optimizer, max_lr=3e-3,
-            #                            steps_per_epoch=int(22000000/128),
-            #                            epochs=1)
-            return optimizer#, [self.scheduler]
+            ## lr find
+            #optimizer = torch.optim.AdamW(self.parameters(), lr=(5e-5 or self.learning_rate))
+
+            #optimizer = torch.optim.AdamW(self.parameters(), lr=1e-5)
+            optimizer = AdamP(self.parameters(), lr=0.0001, betas=(0.9, 0.999), weight_decay=1e-2, nesterov=False)
+            #optimizer = optim.Lamb(self.parameters(), lr= 1e-4, betas=(0.9, 0.999), eps=1e-8, weight_decay=0, )
+            #scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=1e-4, steps_per_epoch=int(22000000/256), epochs=1)
+            return [optimizer]#, [scheduler]
 
     # default used by the Trainer
     checkpoint_callback = ModelCheckpoint(
@@ -274,10 +287,14 @@ if __name__ == '__main__':
         prefix=''
     )
 
-
-
     model = LyftModel(cfg)
 
-    resume_ckpt = "lightning_logs/version_21/checkpoints/N-Step-Checkpoint_2_90000.ckpt"
-    trainer = Trainer(resume_from_checkpoint=resume_ckpt, gpus=1, max_epochs=10, precision=16, gradient_clip_val=0.5, callbacks=callbacks)
+
+    resume_ckpt = "lightning_logs/version_4/checkpoints/N-Step-Checkpoint_3_57500.ckpt"
+    model.load_state_dict(torch.load(resume_ckpt)['state_dict'])
+    ## gonna check the .15pct of the val_set 
+    trainer = Trainer(gpus=1, max_epochs=100, 
+                     precision=16, callbacks=callbacks, limit_train_batches=0.1, gradient_clip_val=1.0)
+                     ## excluding | gradient_clip_val=0.5, | val_check_interval=0.50, test_percent_check=0.1 , auto_lr_find=True
+
     trainer.fit(model, train_loader)
